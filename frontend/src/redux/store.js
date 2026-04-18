fe1cbd8a65c772efa9f943e010f06d3bcf9381d3
Apeksha Ojha <apekshaojha06@gmail.com>
2026-04-18 21:15:00 +0530

    Implement Redux state management and dashboard analytics

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -5,7 +5,7 @@ import transactionReducer from "./slices/transactionSlice";
 import analyticsReducer from "./slices/analyticsSlice";
 import aiReducer from "./slices/aiSlice";
 import adminReducer from "./slices/adminSlice";
-
+// Centralized Redux store configuration
 export const store = configureStore({
   reducer: {
     auth: authReducer,
